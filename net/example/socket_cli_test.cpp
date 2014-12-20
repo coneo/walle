daa1ee69afbf472c8fad26f6b87af0418d8b4bfe
hongxiaoqiang <xqh1007@gmail.com>
2014-12-20 09:54:58 +0800

    完善基本的socket接口，并完善测试用例

---
+++ net/example/socket_cli_test.cpp
@@ -0,0 +1,26 @@
+//#include "net.h"
+#include "socket.h"
+#include "sock_op.h"
+#include <stdio.h>
+
+using namespace walle;
+using namespace walle::net;
+using namespace walle::net::sock_op;
+
+static const uint16_t SERVER_PORT = 9091;
+
+void run_client()
+{
+    int32_t sock = create_socket();
+    Socket cli_socket = Socket(sock);
+
+    Endpoint endpoint(SERVER_PORT);
+    printf("server info ip:%s,port:%u\n", endpoint.getIp().c_str(), endpoint.getPort());
+    cli_socket.connect(&endpoint);
+    printf("connect server success\n");
+}
+
+int main()
+{
+    run_client();
+}
